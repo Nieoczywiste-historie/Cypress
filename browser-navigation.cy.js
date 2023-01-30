29402623f61d1b83a95e803045fc1e00367d1d08
Patryk Kostrzewa <105657544+Nieoczywiste-historie@users.noreply.github.com>
2023-01-30 20:45:53 +0100

    Add files via upload

---
+++ browser-navigation.cy.js
@@ -0,0 +1,35 @@
+
+
+require('cypress-xpath');
+
+describe('Validate webdriver uni homepage links', () => {
+   
+
+
+it("Confirm links redirect to the correct pages", () => {
+   // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+   cy.visit("http://www.webdriveruniversity.com")
+   cy.get("#contact-us").invoke("removeAttr", "target").click({force:true}) //jquery command to remove target which is providing new tab
+   cy.url().should("include", "contactus")
+
+   cy.go("back")
+   cy.reload()
+   cy.url().should("include", "http://www.webdriveruniversity.com")
+//    cy.reload(true) // reload without using cache
+
+
+   cy.go("forward")
+   cy.url().should("include", "contactus")
+
+   cy.go('back')
+   cy.get("#login-portal").invoke("removeAttr", "target").click({force:true}) //jquery command to remove target which is providing new tab
+   cy.url().should("include", "Login-Portal")
+   cy.go('back')
+
+   cy.get("#to-do-list").invoke("removeAttr", "target").click({force:true})
+   cy.url().should("include", "To-Do-List")
+   cy.go("back")
+})
+
+
+})
